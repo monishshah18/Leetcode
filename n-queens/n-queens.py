abfdaadc6b23db515e2aff7555710edb1f9eee75
Monish Shah <monishshah16@gmail.com>
2021-06-18 15:21:49 -0500

    Time: 64 ms (65.37%), Space: 14.6 MB (85.24%) - LeetHub

---
+++ n-queens/n-queens.py
@@ -0,0 +1,19 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+        board = [['.'] * n for _ in range(n)]
+        
+        def place(i: int, vert: int, ldiag: int, rdiag:int) -> None:
+            if i == n:
+                ans.append(["".join(row) for row in board])
+                return
+            for j in range(n):
+                vmask, lmask, rmask = 1 << j, 1 << (i+j), 1 << (n-i-1+j)
+                if vert & vmask or ldiag & lmask or rdiag & rmask: continue
+                board[i][j] = 'Q'
+                place(i+1, vert | vmask, ldiag | lmask, rdiag | rmask)
+                board[i][j] = '.'
+            
+        place(0,0,0,0)
+        return ans
+        
