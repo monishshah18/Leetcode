bf2af04c925af13800cbc3648c3a96e1366e2461
Monish Shah <monishshah16@gmail.com>
2021-07-07 23:20:25 -0500

    Time: 32 ms (84.98%), Space: 14.3 MB (55.54%) - LeetHub

---
+++ multiply-strings/multiply-strings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == '0' or num2 == '0': return '0'
+        n1,n2 = 0,0
+        for i in range(len(num1)):
+            n1 = n1*10 + ord(num1[i]) - 48
+        for j in range(len(num2)):
+            n2 = n2*10 + ord(num2[j]) - 48
+        ans = str(n1*n2)
+        return ans
+        
