c9cbd3c503dfdab825d661c8f976d12cba6710df
Monish Shah <monishshah16@gmail.com>
2021-09-19 21:13:46 -0500

    Time: 52 ms (13.88%), Space: 14 MB (98.91%) - LeetHub

--- binary-tree-right-side-view/binary-tree-right-side-view.py
+++ binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -5,15 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rightSideView(self, root: TreeNode) -> List[int]:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         right_dict = {}
         level = 0
-        self.rightSideRec(root, right_dict, level)
+        self.rightSideViewFunc(root, right_dict, level)
         return right_dict.values()
-    def rightSideRec(self, root, right_dict, level):
+    def rightSideViewFunc(self, root, right_dict, level):
         if not root:
             return
         right_dict[level] = root.val
-        self.rightSideRec(root.left, right_dict, level+1)
-        self.rightSideRec(root.right, right_dict, level+1)
-        
+        self.rightSideViewFunc(root.left, right_dict, level+1)
+        self.rightSideViewFunc(root.right, right_dict, level+1)
