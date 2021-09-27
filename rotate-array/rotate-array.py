d846ac27a0e942b3c42f53c18b2c7385d1bd9d88
Monish Shah <monishshah16@gmail.com>
2021-09-27 12:47:55 -0500

    Time: 228 ms (61.59%), Space: 25.6 MB (53.46%) - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -3,6 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        nums[:] = nums[-k:]+nums[:-k]
-        
+        def twopass(arr,i,j):
+            while i < j:
+                arr[i],arr[j] = arr[j],arr[i]
+                i += 1
+                j -= 1
+            return arr
+        if k > len(nums):
+            k %= len(nums)
+        if k > 0:
+            twopass(nums, 0, len(nums)-1)
+            twopass(nums, 0, k-1)
+            twopass(nums, k, len(nums)-1)
