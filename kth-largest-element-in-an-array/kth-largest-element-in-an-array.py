e3ca6e328e1c838e4ca0153ec086b1f9a3a89237
Monish Shah <monishshah16@gmail.com>
2021-09-27 23:13:00 -0500

    Time: 60 ms (90.80%), Space: 15.1 MB (46.28%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,5 +1,19 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[-k]
-        
+        def partition(l,r):
+            ri = randint(l,r)
+            nums[r],nums[ri] = nums[ri],nums[r]
+            for i,j in enumerate(nums[l:r+1],l):
+                if j >= nums[r]:
+                    nums[l],nums[i] = nums[i],nums[l]
+                    l += 1
+            return l-1
+        l,r,k = 0, len(nums)-1,k-1
+        while True:
+            pos = partition(l,r)
+            if pos < k:
+                l = pos + 1
+            elif pos >  k:
+                r = pos - 1
+            else:
+                return nums[pos]
