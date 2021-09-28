58173305abc0abcf787c0256e60e3d251ee68839
Monish Shah <monishshah16@gmail.com>
2021-09-28 00:20:47 -0500

    Time: 244 ms (94.48%), Space: 16.8 MB (25.80%) - LeetHub

---
+++ subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        sums = 0 #required for jump
+        d = dict()
+        d[0] = 1
+        for i in range(len(nums)):
+            sums += nums[i]
+            count += d.get(sums-k,0)
+            d[sums] = d.get(sums,0) + 1
+        return count
+        
