5365b7cd0cfc1a08307e02503716c06cf1e1133f
Monish Shah <monishshah16@gmail.com>
2021-10-07 08:34:01 -0500

    Time: 6212 ms (64.74%), Space: 14.5 MB (11.68%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,13 +1,13 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows = len(board)
-        cols = len(board[0])
+        rows,cols = len(board),len(board[0])
         for i in range(rows):
             for j in range(cols):
                 if board[i][j]==word[0]:
-                    if self.helper(board, i, j, 0, word)==True:
+                    if self.helper(board,i,j,0,word)==True:
                         return True
         return False
+    
     def helper(self, board, row, cols, ind, word):
         if len(word)==ind:
             return True
@@ -17,11 +17,10 @@ class Solution:
             return False
         temp = board[row][cols]
         board[row][cols] = '.'
-        left = self.helper(board,row+1, cols,ind+1,word)
-        right = self.helper(board,row-1, cols,ind+1,word)
-        up = self.helper(board,row, cols+1,ind+1,word)
-        down = self.helper(board,row, cols-1,ind+1,word)
-        
+        up = self.helper(board, row, cols+1, ind+1, word)
+        down = self.helper(board, row, cols-1,ind+1,word)
+        left = self.helper(board, row+1, cols, ind+1, word)
+        right = self.helper(board, row-1, cols, ind+1, word)
         found = (left or right or up or down)
         board[row][cols] = temp
         
