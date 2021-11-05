2ed76bcb221c997a4f4f83f25212199da5b0e2d6
Monish Shah <monishshah16@gmail.com>
2021-11-05 09:39:27 -0500

    Time: 36 ms (82.51%), Space: 14.3 MB (7.29%) - LeetHub

--- arranging-coins/arranging-coins.py
+++ arranging-coins/arranging-coins.py
@@ -1,4 +1,14 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        return int((-1+(sqrt(1+(8*n))))/2)
+        low,high = 1,n
+        while low <= high:
+            mid = (low+high)//2
+            total = mid*(mid+1)//2
+            if total == n:
+                return mid
+            elif total > n:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return high
         
