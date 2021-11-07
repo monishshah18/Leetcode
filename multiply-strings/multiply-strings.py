cec1bb871c3ed2ea992133abf26c42db05b30073
Monish Shah <monishshah16@gmail.com>
2021-11-06 22:05:16 -0500

    Time: 52 ms (63.55%), Space: 14 MB (99.11%) - LeetHub

--- multiply-strings/multiply-strings.py
+++ multiply-strings/multiply-strings.py
@@ -4,8 +4,7 @@ class Solution:
         n1,n2 = 0,0
         for i in range(len(num1)):
             n1 = n1*10 + ord(num1[i]) - 48
-        for j in range(len(num2)):
-            n2 = n2*10 + ord(num2[j]) - 48
+        for i in range(len(num2)):
+            n2 = n2*10 + ord(num2[i]) - 48
         ans = str(n1*n2)
         return ans
-        
