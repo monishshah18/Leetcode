02ad8335dd78b9d1315d422a6b81113391dbfbee
Monish Shah <monishshah16@gmail.com>
2021-11-07 22:11:59 -0600

    Time: 28 ms (86.81%), Space: 14.5 MB (15.60%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.py
+++ unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,15 +1,15 @@
 class Solution:
     def numTrees(self, n: int) -> int:
         dp = [[None]*(n+1) for _ in range(n+1)]
-        def count(start,end):
+        def count(start, end):
             if start >= end:
                 return 1
             if dp[start][end]:
                 return dp[start][end]
             total = 0
             for i in range(start, end+1):
-                total += count(start,i-1)*count(i+1,end)
+                total += count(start, i-1)*count(i+1, end)
             dp[start][end] = total
             return dp[start][end]
-        res = count(1,n)
+        res = count(1, n)
         return res
