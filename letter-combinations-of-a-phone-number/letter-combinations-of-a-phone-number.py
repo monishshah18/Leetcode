f55e20900d650c625e03307a8653265457d4050d
Monish Shah <monishshah16@gmail.com>
2021-11-10 19:21:57 -0600

    Time: 28 ms (88.49%), Space: 14.4 MB (32.95%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        digitDict = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
-        if len(digits) == 0:
+        digitMap = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        if len(digits)==0:
             oldList = []
         else:
             oldList = ['']
             for i in digits:
                 newList = []
-                for j in digitDict[i]:
+                for j in digitMap[i]:
                     newList.extend([k+j for k in oldList])
                 oldList = newList
         return oldList
